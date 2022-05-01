f74ee3146912d808e1b87305f0b7f43f99004cd6
eshazine <eshazine@gmail.com>
2022-05-01 06:04:15 +0300

    Esra part 1

--- src/Esra1.java
+++ src/Esra1.java
@@ -1,5 +1,6 @@
 public class Esra1 {
     public static void main(String[] args) {
         System.out.println("I rose in an unGodly hour to tap my skills in Github");
+        System.out.println("She is beyond a shadow of doubt is a fair weather friend ");
     }
 }
