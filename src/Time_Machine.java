329644c8e088497772a46076208cfee2ab37fec1
eshazine <eshazine@gmail.com>
2022-05-01 05:55:46 +0300

    time machine 3

--- src/Time_Machine.java
+++ src/Time_Machine.java
@@ -2,5 +2,6 @@ public class Time_Machine {
     public static void main(String[] args) {
         System.out.println("I have my head in the clouds");
         System.out.println("I feel i little bit under the weather");
+        System.out.println("I am saving money fro a rainy day");
     }
 }
