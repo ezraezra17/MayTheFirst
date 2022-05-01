ccdb0c5ab0bc2cac688e857712109a898ee2ced2
eshazine <eshazine@gmail.com>
2022-05-01 05:19:16 +0300

    commit3

--- src/May1.java
+++ src/May1.java
@@ -3,5 +3,6 @@ public class May1 {
     public static void main(String[] args) {
         System.out.println("I am  getting better and better everyday");
         System.out.println("they dragged out the lesson around 1 hour");
+        System.out.println("She is stoking the fire with a hair drier");
     }
 }
