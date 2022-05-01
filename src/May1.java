b73012eef889c325d9eb1239138b516963028785
ezraezra17 <99948953+ezraezra17@users.noreply.github.com>
2022-05-01 06:21:46 +0300

    Update May1.java

--- src/May1.java
+++ src/May1.java
@@ -4,5 +4,6 @@ public class May1 {
         System.out.println("I am  getting better and better everyday");
         System.out.println("they dragged out the lesson around 1 hour");
         System.out.println("She is stoking the fire with a hair drier");
+        // these are changes from github
     }
 }
